4796508e90643d517b38a75b2661601912ddc355
bmar116 <bmar116@hotmail.com>
2023-11-08 05:11:17 +0000

    fix logging and cleanup after install

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ mv ./scripts/* ${SERVER_ROOT}/${SCRIPT_DIR}
 MINECRAFT_SCRIPT_DIR=${SERVER_ROOT}/${SCRIPT_DIR}
 export MINECRAFT_SCRIPT_DIR
 wget -O /tmp/bedrock_server-latest.zip ${BDS_LATEST_VERSION} && unzip -d ${SERVER_ROOT}/${SERVER_DIR}/ /tmp/bedrock_server-latest.zip
-rm /tmp/bedrock_server-latest.zip
+cd ~
+rm -r /tmp/bds-utils /tmp/bds-utils.zip /tmp/bedrock_server-latest.zip
 minecraft start
 minecraft login
--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,7 @@ while true; do # infinite repeating loop keeps server unlive unless killed
   # run server, when server shuts down backup log file and restart after 5 seconds
   cd ${SERVER_ROOT}/${SERVER_DIR}
   LD_LIBRARY_PATH=. ./bedrock_server
-  mv ${SERVER_ROOT}/${LOG_DIR}/latest.log "$LOG_DIR/$(date +'%Y%m%d-%H%M%S').log" && touch ${SERVER_ROOT}/${LOG_DIR}/latest.log
+  mv ${SERVER_ROOT}/${LOG_DIR}/latest.log "${SERVER_ROOT}/${LOG_DIR}/$(date +'%Y%m%d-%H%M%S').log" && touch ${SERVER_ROOT}/${LOG_DIR}/latest.log
   echo "Waiting 5 seconds to restart unless CTRL-C is pressed....."
   sleep 5
   clear
